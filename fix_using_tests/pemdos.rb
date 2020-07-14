b9be4a98231df5093fcc5a41ea2cc31dac7a2c73
Samir Triande <habib.triande@gmail.com>
2020-07-14 19:30:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] = "s"
-    "s" * 10 + string
+    "s" * 10 + string # Put the string before the integer for the multiplication.
   else
     string
   end
