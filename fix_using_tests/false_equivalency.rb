3c56811683f0da7f9303d2b6ea39149cd8150ab9
Samir Triande <habib.triande@gmail.com>
2020-07-14 18:25:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   num = 1
-  if num = 1
+  if num == 1
      puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      puts "HAM HAM HAM IN MY TUMMY"
   end
   #binding.pry
